4abd4127e509e3f0e2e1f89cb6d85b0b0ef92b2c
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2023-08-17 11:09:05 -0300

    logo alianza-horda

--- src/app/components/table-data/table-data.component.ts
+++ src/app/components/table-data/table-data.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { TableRates } from 'src/app/models/currency.model';
 import { TokenInfo } from 'src/app/models/wow-token.model';
 
 @Component({
   selector: 'app-table-data',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './table-data.component.html',
   styles: [],
 })
 export class TableDataComponent implements OnInit {
   @Input({ required: true }) dolarValue!: TableRates;
   @Input({ required: true }) wowToken!: TokenInfo;
+
+  imageSrc = "https://external-preview.redd.it/5GMy4M7EHF4sqzvzqqAjK3wZBCo1XaJtIlXcHHcRrFQ.png?width=640&crop=smart&auto=webp&s=d6135840c58bd2a0eb579987799349c9c12e5317";
 
   pesoGold: number = 0;
   dolarGold: number = 0;
