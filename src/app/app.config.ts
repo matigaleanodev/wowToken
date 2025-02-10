a88dd1f44458292f02d548d6f459f309775f8212
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-09 21:25:22 -0300

    actualizacion angular 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeAr from '@angular/common/locales/es-AR';
